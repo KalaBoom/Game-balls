3a2afbf8e72a772c21a9d4fd3e456ac69fcc67e9
Eugene <gaster334@gmail.com>
2020-08-07 12:29:20 +0300

    remove useless files

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 const 
-    {series, parallel} = require('gulp')
+    {series, parallel} = require('gulp'),
     clean = require('./tasks/del'),
-    styles = require('./tasks/styles')
-    scripts = require('./tasks/scripts')
-    pages = require('./tasks/pages')
+    styles = require('./tasks/styles'),
+    scripts = require('./tasks/scripts'),
+    pages = require('./tasks/pages'),
     browser = require('./tasks/browser')
-    images = require('./tasks/image')
-    fonts = require('./tasks/fonts')
 
-const dev = parallel(pages, styles, scripts, images, fonts)
+const dev = parallel(pages, styles, scripts)
 const build = series(clean, dev)
 
 module.exports.dev = series(build, browser)
--- tasks/fonts.js
+++
@@ -1,7 +0,0 @@
-const
-    {src, dest} = require('gulp')
-
-module.exports = function pages() {
-    return src('src/fonts/*')
-        .pipe(dest('build/webfonts'))
-}
